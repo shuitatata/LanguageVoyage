6f62776c65542982deb9cbe52eddd8603bc53d18
Wei Lai <2462703357@qq.com>
2025-05-15 15:47:37 +0800

    chore: 修复ESLint配置
    - 更新Node.js版本到22.x
    - 修复ESLint配置文件语法
    - 使用新的ESLint配置方式
    - 移除不支持的命令行选项

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import vue from "eslint-plugin-vue";
 import typescript from "@typescript-eslint/eslint-plugin";
 import typescriptParser from "@typescript-eslint/parser";
 
-export default defineConfig([
+export default [
+    {
+        // 全局忽略配置
+        ignores: [
+            "**/node_modules/**",
+            "**/dist/**",
+            "**/.git/**",
+            "**/.vscode/**",
+            "**/.idea/**",
+            "**/coverage/**"
+        ]
+    },
     // Vue文件配置
     {
         files: ["**/*.vue"],
@@ -45,4 +55,4 @@ export default defineConfig([
             ...js.configs.recommended.rules,
         },
     },
-]); 
+]; 
